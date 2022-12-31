00053fc80a00204ef8ceb3aec0acb8f797189432
Jake Lees <brocococonut@gmail.com>
2022-12-31 17:37:06 +1100

    Initial commit

---
+++ demo.ts
@@ -0,0 +1,14 @@
+import { LocaleKit } from "./mod.ts";
+
+const locale = new LocaleKit({
+  languages: {
+    en: {
+      test: {
+        key: "Test Key",
+      },
+    },
+  },
+  fallback_language: "en",
+});
+
+console.log(locale.t("test.key"));
